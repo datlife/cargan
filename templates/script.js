902c1b5e0f4ae830dddafa77f1ddd02cd11c2b7e
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-02-13 12:54:32 -0800

    Added scores

--- templates/script.js
+++ templates/script.js
@@ -18,7 +18,8 @@ function _move(curr_canvas, event){
 function _draw_image(curr_canvas, x_offset, y_offset, zoom_level) {
 
     var ctx = curr_canvas.getContext("2d");
-    var detections = JSON.parse(curr_canvas.getAttribute('detections')).bboxes;
+    var bboxes = JSON.parse(curr_canvas.getAttribute('detections')).bboxes;
+
     var img_title = curr_canvas.getAttribute('src').split('/').pop();
     var img = new Image();
 
@@ -26,25 +27,30 @@ function _draw_image(curr_canvas, x_offset, y_offset, zoom_level) {
         ctx.clearRect(0, 0, curr_canvas.width, curr_canvas.height);
         var x_top =  x_offset*(img.width - img.width / zoom_level);
         var y_top =  y_offset*(img.height - img.height/ zoom_level);
+
         ctx.drawImage(img,
             x_top ,
             y_top,
             img.width / zoom_level, img.height / zoom_level,
             0, 0, curr_canvas.width, curr_canvas.height);
 
-        if (detections) {
+        if (bboxes) {
+            var confidence_scores = JSON.parse(curr_canvas.getAttribute('detections')).scores;
+
             ctx.beginPath();
             var w_ratio = zoom_level*(curr_canvas.width / img.width);
             var h_ratio = zoom_level*(curr_canvas.height / img.height);
             var scale_array = [w_ratio, h_ratio, w_ratio, h_ratio];
 
-            Object.keys(detections).map(function (bbox_id) {
-                bbox = detections[bbox_id].map((value, idx) => value * scale_array[idx]);
-                bbox = [bbox[0] -  x_offset*(curr_canvas.width),
-                        bbox[1] - y_offset*(curr_canvas.height),
-                        (bbox[2] - bbox[0]),
-                        (bbox[3] - bbox[1])];
-                ctx.rect(bbox[0], bbox[1], bbox[2], bbox[3]);
+            Object.keys(bboxes).map(function (bbox_id) {
+                bbox = bboxes[bbox_id].map((value, idx) => value * scale_array[idx]);
+                xpos = bbox[0] -  x_offset*(curr_canvas.width);
+                ypos = bbox[1] - y_offset*(curr_canvas.height);
+                width = bbox[2] - bbox[0];
+                height = bbox[3] - bbox[1];
+
+                ctx.rect(xpos, ypos, width, height);
+                drawTextBG(ctx, confidence_scores[id], '32px arial',  xpos, ypos - 20);
             });
             ctx.strokeStyle = 'yellow';
             ctx.lineWidth = 2;
@@ -55,4 +61,20 @@ function _draw_image(curr_canvas, x_offset, y_offset, zoom_level) {
     };
     img.crossOrigin = true;
     img.src = curr_canvas.getAttribute('src');
+}
+
+function drawTextBG(ctx, txt, font, x, y) {
+
+    ctx.save();
+    ctx.font = font;
+    ctx.textBaseline = 'top';
+    ctx.fillStyle = 'black';
+
+    var width = ctx.measureText(txt).width;
+    ctx.fillRect(x, y, width, parseInt(font, 10));
+
+    ctx.fillStyle = 'yellow';
+    ctx.fillText(txt, x, y);
+
+    ctx.restore();
 }
