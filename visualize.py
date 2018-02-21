4332a0314b96afc7ca94cf586fcb803089e1b871
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-02-20 21:33:08 -0800

    Added regex

--- visualize.py
+++ visualize.py
@@ -1,9 +1,10 @@
+import re
 import flask
 import pandas as pd
 from flask_paginate import Pagination, get_page_args
 from cargan.utils.parser import load_data, flatten_dict
 
-app  = flask.Flask(__name__, static_url_path="", static_folder="IPCam")
+app  = flask.Flask(__name__, static_url_path="", static_folder="/media/dat/dataset/DETRAC/OUTPUT")
 
 # Pagination Cfg
 PER_PAGE = 6
@@ -11,7 +12,7 @@ PER_PAGE = 6
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    data = flatten_dict(load_data("IPCam"), current_level=0, max_level=1)
+    data = flatten_dict(load_data("/media/dat/dataset/DETRAC/OUTPUT", load_gt=True), current_level=0, max_level=1)
     current_page, per_page, offset = get_page_args()
     pagination = Pagination(total=int(len(data)),
                             page=current_page,
@@ -31,7 +32,7 @@ def index():
 
 def custom_list_sort(a_list, reverse=False):
     return sorted(a_list,
-                  key=lambda x: int(x.split('/')[-1].split('.')[0]),
+                  key=lambda img_file: int(re.findall('\d+', img_file.split('/')[-1].split('.')[0])[0]),
                   reverse=reverse)
 
 if __name__ == '__main__':
